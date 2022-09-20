95a44a0422a200d938db4e8704547442ce0ca1c7
gabrieleaquaro <gabriaqua@hotmail.it>
2022-09-20 22:17:30 +0200

    minor

--- portfolio/src/ProjectPage.js
+++ portfolio/src/ProjectPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Carousel, Container, Image } from "react-bootstrap";
+import { Carousel, Image } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { MyFooter } from "./components/footer";
 import data from "./data.json";
@@ -21,7 +21,7 @@ export default function ProjectPage(props) {
         <div className="position-relative large-padding img-bg generic-bg">
           <div className="overlay-mf"></div>
 
-          <Container className="position-relative">
+          <div className="container position-relative">
             <div className="row">
               <div className="col-12 text-center dark-color">
                 <h2 className="main-title"> Project Information </h2>
@@ -35,70 +35,62 @@ export default function ProjectPage(props) {
                 </span>
               </div>
             </div>
-          </Container>
+          </div>
         </div>
 
-        <div className="row d-flex justify-content-center mt-5 mb-3 ">
-          <div className="col-md-6 d-flex justify-content-center ">
-            <Carousel className="cust-carousel">
-              {projectData.imgs.map((img, index) => {
-                return (
-                  <Carousel.Item
-                    key={img + "_" + index}
-                    className="align-middle"
-                  >
-                    <Image
-                      src={projectImages[img]}
-                      alt="Image"
-                      className="project-image"
-                    />
-                  </Carousel.Item>
-                );
-              })}
-            </Carousel>
-          </div>
-
-          <div className="col-md-4 h-100">
-            <div className="row light-bg">
-              <span className="overview-title mb-3"> OVERVIEW </span>
-              <div>
-                <span>
-                  <p>
-                    <b>Category:</b>&nbsp;
-                    {projectData.category}
-                  </p>
-                </span>
-                <span>
-                  <p>
-                    <b>Date:</b>&nbsp;{projectData.date}
-                  </p>
-                </span>
-                {projectData.url && (
-                  <span>
-                    <p>
-                      <b>Url:</b>&nbsp;
-                      <a href={projectData.url}> {projectData.url}</a>
-                    </p>
-                  </span>
-                )}
-              </div>
+        <div className="container position-relative">
+          <div className="row  mt-5 mb-3 ">
+            <div className="col-md-6  ">
+              <Carousel className="cust-carousel">
+                {projectData.imgs.map((img, index) => {
+                  return (
+                    <Carousel.Item
+                      key={img + "_" + index}
+                      className="align-middle"
+                    >
+                      <Image
+                        src={projectImages[img]}
+                        alt="Image"
+                        className="project-image"
+                      />
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
             </div>
 
-            <div className="row light-bg mt-5">
-              <h2 className="overview-title">
-                {projectData["long-title"].toUpperCase()}
-              </h2>
-              <p>
-                {projectData.description ||
-                  `Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
-                Cras ultricies ligula sed magna dictum porta. Quisque velit
-                nisi, pretium ut lacinia in, elementum id enim. Praesent sapien
-                massa, convallis a pellentesque nec, egestas non nisi.
-                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-                posuere cubilia Curae; Donec velit neque, auctor sit amet
-                aliquam vel, ullamcorper sit amet ligula. Nulla quis lorem ut
-                libero malesuada feugiat.`}
-              </p>
+            <div className="col-md-6 h-100">
+              <div className="row light-bg">
+                <span className="overview-title mb-3"> OVERVIEW </span>
+                <div>
+                  <span>
+                    <p>
+                      <b>Category:</b>&nbsp;
+                      {projectData.category}
+                    </p>
+                  </span>
+                  <span>
+                    <p>
+                      <b>Date:</b>&nbsp;{projectData.date}
+                    </p>
+                  </span>
+                  {projectData.url && (
+                    <span>
+                      <p>
+                        <b>Url:</b>&nbsp;
+                        <a href={projectData.url}> {projectData.url}</a>
+                      </p>
+                    </span>
+                  )}
+                </div>
+              </div>
+
+              <div className="row light-bg mt-5">
+                <h2 className="overview-title mb-4">
+                  {projectData["long-title"].toUpperCase()}
+                </h2>
+                <p>{projectData.description}</p>
+              </div>
             </div>
           </div>
         </div>
